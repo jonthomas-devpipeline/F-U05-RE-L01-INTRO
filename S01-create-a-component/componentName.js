7a8d7e9cd72ee8f567b442d4bddcfb1d1abea46d
jonthomas-devpipeline <102564782+jonthomas-devpipeline@users.noreply.github.com>
2022-12-06 15:26:57 -0700

    Create componentName.js

---
+++ S01-create-a-component/componentName.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+
+export default class ComponentName extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            toggle: false
+          };  
+        
+        this.handleToggleClick = this.handleToggleClick.bind(this);
+        }
+
+    componentMethodOne() {
+        //JS code
+    }
+
+    componentMethodTwo() {
+        // JS code
+    }
+
+    render() {
+        return(
+            <div className="App">
+                <h1>Component Heading</h1>
+            </div>
+        );
+    }
+}
